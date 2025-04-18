893ce9078b8a080d53cc30bcca0383ecc70a9265
Gabriel Linard <127212969+Gabrilinard@users.noreply.github.com>
2025-04-18 19:32:49 -0300

    up

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: 'http://fisiopilattes.netlify.app',  // Permitir seu domínio
+  origin: 'https://fisiopilattes.netlify.app',  // Permitir seu domínio
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,  // Se for usar cookies/autenticação
