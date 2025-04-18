5878c36b3414dbfcd9411ed1441a29e7276f0085
Gabriel Linard <127212969+Gabrilinard@users.noreply.github.com>
2025-04-18 12:33:44 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/agendarAqui/', // <-- Nome exato do repositório
   plugins: [react()],
 })
