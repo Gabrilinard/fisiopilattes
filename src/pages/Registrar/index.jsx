bf4c5eee76dd190deb8bbca45be1c9d22c44aaa1
Gabriel Linard <127212969+Gabrilinard@users.noreply.github.com>
2025-04-18 17:44:48 -0300

    up

--- src/pages/Registrar/index.jsx
+++ src/pages/Registrar/index.jsx
@@ -108,7 +108,7 @@ const Registro = () => {
     }
   
     try {
-      const response = await axios.post('http://localhost:4000/register', { nome, sobrenome, telefone, email, senha });
+      const response = await axios.post('http://fisiopilattes.up.railway.app/register', { nome, sobrenome, telefone, email, senha });
       console.log(response.data); // Verificar a resposta
       alert('Usuário cadastrado com sucesso!');
       navigate('/Entrar');
